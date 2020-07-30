74cea90aa2c31644199b1803fa2db2d1491c5800
mayureshudas <mayuresh.udas@capgemini.com>
2020-07-30 18:28:05 +0530

    Update run.groovy

--- vars/run.groovy
+++ vars/run.groovy
@@ -1,4 +1,4 @@
 def runproject() {
-       sh "java -cp target/jb-hello-world-maven-0.1.0.jar src/main/java/hello/HelloWorld"
+       sh "java -cp target/jb-hello-world-maven-0.1.0.jar /src/main/java/hello/HelloWorld"
 }
 return this
